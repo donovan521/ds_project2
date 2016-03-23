38c772d5fcec06ff1d807154e6df02c11baab1d9
donovan521 <donovan521@users.noreply.github.com>
2016-03-23 15:51:34 -0400

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -4,6 +4,7 @@
 #include <cerrno>
 #include <string>
 #include <sstream>
+#include <fstream>
 #include <fcntl.h>
 #include <unistd.h>
 #include "node.h"
@@ -27,7 +28,33 @@ int main(const int argc, const char * argv []) {
     exit(0);
   } // if
 
+  string filename = argv[1];
+  ifstream input(filename);
+  int r =0,flag = 0;
+  for(string stringIn; getline(input,stringIn);) { //go thru each line
+    istringstream iss(stringIn);
+    string uinput;
+    while(getline(iss,uinput,' ')){
+      if (flag==1){
+	int valN = stoi(uinput);
+	//insert
+	flag=0;
+      }
+      if(flag==2){
 
+	flag=0;
+      }
+      if(uinput=="insert"){
+	flag = 1;
+      }
+      if(uinput=="delete"){
+        flag = 2;
+      }
+
+
+    }
+  
+}
 
   return EXIT_SUCCESS;
 } // main
